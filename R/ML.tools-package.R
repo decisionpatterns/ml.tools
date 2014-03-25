cdba1554cc2130ba8062578a4b8d81e1224ee5c2
Christopher Brown <cbrown@decisionpatterns.com>
2014-03-24 21:42:31 -0700

    Add DESCRIPTION ML.tools-package.R .gitignore

---
+++ R/ML.tools-package.R
@@ -0,0 +1,9 @@
+#' ML.tools - Machine Learning Tools
+#' 
+#' The \code{ML.tools} packages provides a number of tools that are generally
+#' applicable for Machine learning applications.
+#' 
+#' @author Christopher Brown
+#' @docType package
+
+NULL
