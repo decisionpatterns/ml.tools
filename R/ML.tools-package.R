b0cdea75d4f34eaa603e64192835686d6e22abda
Christopher Brown <cbrown@decisionpatterns.com>
2014-03-24 21:48:28 -0700

    Fix Documentation

--- R/ML.tools-package.R
+++ R/ML.tools-package.R
@@ -3,6 +3,9 @@
 #' The \code{ML.tools} packages provides a number of tools that are generally
 #' applicable for Machine learning applications.
 #' 
+#' @name ML.tools
+#' @examples
+#'   # -tk
 #' @author Christopher Brown
 #' @docType package
 
