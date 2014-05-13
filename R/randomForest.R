114101fdafc63ac8fee576e243d0c3e359703e60
Christopher Brown <cbrown@decisionpatterns.com>
2014-05-13 01:21:11 -0700

    Add randomForest.R

---
+++ R/randomForest.R
@@ -0,0 +1,74 @@
+#' parallelRandomForest
+#'
+#' provides parallelRandomForest and a parallel drop-in replacement for 
+#' \code{randomForest}using foreach/doParallel
+#' 
+#' @param ... parameters passed to \code{randomForest}
+#' @param ntree integer; number of trees train
+#' 
+#' Trains a Random Forest model using a \code{foreach} cluster. The cluster must
+#' first be registered with \code{registerDoParallel} 
+#'
+#' @return A \code{randomForest} object.
+#' 
+#' @references 
+#'   http://stackoverflow.com/questions/5571774/what-is-the-easiest-way-to-parallelize-a-vectorized-function-in-r/15414748#15414748 
+#'
+#' @seealso 
+#'   \code{\link[randomForest]{randomForest}}, 
+#'   code{\link[caret]{train}}, 
+#'   \code{\link[doParallel]{registerDoParallel}},
+#'   \code{\link[foreach]{foreach}}
+#'     
+#' @examples
+#'   registerDoParallel(4)
+#'   
+#'   #EXPLICIT:
+#'   fit <- parallelRandomForest( Species ~ . , iris, ntree=200)
+#'   
+#'   # DROP-IN REPLACEMENT
+#'   fit <- randomForest( Species ~ . , iris, ntree=200)
+#'   
+#'   # CARET 
+#'   fit <- train( Species ~ . , iris, ntree=200 )
+#'   
+#' @rdname randomForest
+#' @export
+
+parallelRandomForest <- function( ..., ntree=500 ) { 
+
+  # require(itertools)
+  require(randomForest)
+  require(foreach)
+  require(doParallel)
+  
+  # CALCULATE THE NUMBER OF TREES ON EACH WORKER
+    nworkers <- getDoParWorkers()
+    reps <- rep( floor( ntree/nworkers ), nworkers )
+    mod  <- ntree %% nworkers 
+    if( mod > 0 ) reps[1:mod] <- reps[1:mod] + 1
+    reps <- reps[ reps > 0 ]  
+  
+  fit <- 
+    foreach( 
+        ntree=reps
+        , .combine=randomForest::combine
+        , .multicombine=TRUE 
+    ) %dopar% 
+      randomForest::randomForest( ..., ntree=ntree )
+
+  return(fit)
+  
+}
+
+
+
+
+#' @rdname randomForest
+#' @export
+randomForest <- function( ... ) { 
+  # message( "...using ML.tools::randomForest" ) 
+  parallelRandomForest(...)
+}
+
+
