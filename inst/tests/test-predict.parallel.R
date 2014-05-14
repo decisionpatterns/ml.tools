7f0e2c1a2dc5c141fb9fb7c569835cc08784f808
Christopher Brown <cbrown@decisionpatterns.com>
2014-05-14 15:01:45 -0700

    Add inst/tests/test-predict.parallel.R

---
+++ inst/tests/test-predict.parallel.R
@@ -0,0 +1,31 @@
+library(testthat)
+library(ML.tools)
+library(doParallel)
+  registerDoParallel(4)
+
+#' require(randomForest) 
+data(iris)
+
+# Linear Model
+  fit  <- lm(Sepal.Length  ~ ., iris)
+   
+  p  <- predict( fit, iris )
+  pp <- predict.parallel( fit, iris )
+    
+  expect_true( 
+    all(p== pp)
+    , info="predict.pararallel is not equivalent to predict for lm" 
+  )
+
+# message randomForest
+  require(randomForest)
+  
+  fit  <- randomForest( Sepal.Length  ~ ., iris )
+  
+  p  <- predict( fit, iris )
+  pp <- predict.parallel( fit, iris )
+    
+  expect_true( 
+    all(p== pp)
+    , info="predict.pararallel is not equivalent to predict for randomForest" 
+  )
