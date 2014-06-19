9c032ae9685a1ed3477a39c26001798a61733afa
Christopher Brown <cbrown@decisionpatterns.com>
2014-06-19 11:35:19 -0700

    Add window_expr a helper for  generating an expression for window features

---
+++ R/window_expr.R
@@ -0,0 +1,46 @@
+#' window_expr
+#' 
+#' generate an expression that can be \code{eval}'d on the event 
+#' data set to keep only the window of relevant events to the features
+#'
+#' @param milestone character; name of milestone_dt column 
+#' @param event_dt character; name of date column related to the event
+#' @param direction; before, after, within 
+#' @param span length of time for the window 
+#' 
+#' Window := direction + span 
+#' 
+#' before: Milestone - event_dt <= window  (LEADING INDICATOR)
+#' after : Milestone - event_dt >= window  (LAGGING INDICATOR)
+#' within (of): abs(Milestone - event_dt ) <= windows     
+#' 
+#' @return 
+#'   An unevalutated call object that can be evaluated in the milestone-event dataset.
+#' 
+#' @examples
+#'   window_expr( "milestone_dt", "event_dt", ddays(2) )
+#'   window_expr( "milestone_dt", "event_dt", ddays(3), "within" )
+#'   window_expr( "milestone_dt", "event_dt", ddays(4), "before" )
+#'   window_expr( "milestone_dt", "event_dt", ddays(5), "after" )
+#'   
+#' @rdname   
+
+window_expr <- function( milestone, event_dt, direction="of", span ) {
+  
+  span <- as.numeric(span)
+  
+  template <- switch( direction  
+      , before = 'diffdays( {{milestone}}, {{event_dt}} ) <= {{span}}'
+      , after  =  'diffdays( {{event_dt}}, {{milestone}} ) <= {{span}}'
+      , of = 'abs( diffdays( {{milestone}}, {{event_dt}} ) ) <= {{span}}' 
+  )
+  
+  tdata <- list( 
+      milestone = milestone
+    , event_dt = event_dt    # name of column on the event data set
+    , span = span
+  )
+  
+  return( parse( text=whisker.render( template, tdata ) )[[1]] )
+  
+}
