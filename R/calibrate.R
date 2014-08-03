93cf5f4b62d1bbd10dcc7f53bb8b9884ed94f145
twillms <twillms@h04.(none)>
2014-08-03 16:47:07 -0700

    Updated the documentation.

--- R/calibrate.R
+++ R/calibrate.R
@@ -1,40 +1,36 @@
 #' Calibrate 
 #' 
-#' Calculates one modelled distribution to closely match the original input distribution
+#' Calibrates a modelled distribution so that it closely matches the 
+#' original distribution that was used as input into the model.
 #' 
-#' @param x numeric; original distribution 
-#' @param y numeric; modeled distribution
-#' @param f function; function used to calculate number of bins used to separate original distribution
+#' @param x numeric; original distribution in the form of a vector
+#' @param y numeric; modeled distribution in the form of a vector
 #'
-#' Default bin calculation uses Sturges formula - 
-#' Uses linear model to determine the calibrated ...
-#' 
+#' The function first sorts the distributions then ensures they're the same size
+#' then uses approx to linear interpolate the modelled distribution into the 
+#' same form as the original distribution.
+#'  
 #' @return numeric vector; y transformed into basis of x
-#' @seealso reference any functions that are input links to other functions using latex 
+#' @seealso sort, seq, approx 
 #' @examples 
 #'    org_distribution <- rlnorm(1000) 
 #'    modelled_distribution <- rnorm(1000,mean=5)
 #'    cal_dist <- calibrate(org_distribution,modelled_distribution)
+#'    calibrate(1:10, 1:10)
+#'    calibrate(1:10, 1:20)
+#'    calibrate(1:20, 1:10)
 #' @rdname calibrate
 #' @export
 #' @aliases calibrate
 #' 
 calibrate <- function(x, y){
 
-  if(length(x) > length(y)){
-    l <- length(y)
-    x <- sample(x, l, replace = TRUE)
-  } else {
-    l <- length(x)
-    y <- sample(y,l, replace = TRUE)
-  }
-  
-  #x = sort(x)
-  #y = sort(y)
-  #df <- data.frame(x,y)
-  #mod <- lm(y ~ x, df)
-  
-  calibrated <- approx(x,y, rule=c(2,2), method='constant')
+  x = sort(x)
+  y = sort(y)
+  x = x[seq(1, length(x), length.out = min(length(x),length(y)))]
+  y. = y[seq(1, length(y), length.out = min(length(x),length(y)))]
+
+  calibrated <- approx(y.,x, xout=y, rule=c(2,2), method='constant')
   
   return (calibrated$y)
   
