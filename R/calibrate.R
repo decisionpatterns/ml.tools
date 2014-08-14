e465601216a4eb1e6d0775f355ca958efd0ca3ca
Christopher Brown <cbrown@decisionpatterns.com>
2014-08-14 09:40:54 -0700

    Improve calibrate.R; y. temporary variable not needed.

--- R/calibrate.R
+++ R/calibrate.R
@@ -38,7 +38,7 @@
 #'   calibrate(1:10, 1:5)  # 1 1 2 2 3 3 4 4 4 5
 #'   calibrate(10:1, 1:5)  # 5 4 4 4 3 3 2 2 1 1
 #'   
-#'   calibrate( rnorm(1000,mean=5), rlnorm(1000) )
+#'   calibrate( rnorm(100,mean=5), rlnorm(2000) )
 #'   
 #'   \dontrun{
 #'   
@@ -55,14 +55,14 @@
 
 calibrate <- function(x, y, method="constant", rule=2,  ... ){
 
-  x. = sort(x)
-  y. = sort(y)
+  x. <- sort(x)
+  y  <- sort(y)
   
   # Make both vectors the length of the shorter 
-  x. = x.[ seq( 1, length(x), length.out = min( length(x), length(y)) ) ]
-  y. = y.[ seq( 1, length(y), length.out = min( length(x), length(y)) ) ]
+  x. <- x.[ seq( 1, length(x), length.out = min( length(x), length(y)) ) ]
+  y  <- y[ seq( 1, length(y), length.out = min( length(x), length(y)) ) ]
 
-  calibrated <- approx( x., y., xout=x, method=method, rule=rule, ...  )
+  calibrated <- approx( x., y, xout=x, method=method, rule=rule, ...  )
   
   return (calibrated$y)
   
