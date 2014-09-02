01fa02ada6c787eb668d8a5cc222ce5a50ed0c6f
Christopher Brown <cbrown@decisionpatterns.com>
2014-09-02 14:09:23 -0700

    Version 0.16.1
    Remove date features dependence on TimeWarp; Uses events packages instead

--- R/date-features.R
+++ R/date-features.R
@@ -3,17 +3,32 @@
 #' Additional functions 
 #'
 #' @param dates dates
-#' @param type  (passed to TimeWarp::holidays)
+#' @param ... passed to events::major_dates
 #' 
+#' \code{days_to_holiday} calculates the days to the nearest event. \cr
+#' \code{days_from_holiday} calculates the days from the most recent event \cr
+#' 
+#' @examples
+#'   days_to_holiday( today() )
+#'   
 #' @export
 #' @rdname date-extras
 
-days_to_holiday <- function( dates, type="NYSE" ) {
+days_to_holiday <- function( dates, ... ) {
   
-  yrs <- unique( year(dates) )
-  yrs <- c( min(yrs)-1, yrs, yrs+1 )
+  data(events)
+  
+  # dates <- as.POSIXct( years )
+  # if( ! is.POSIXct(dates) ) dates <- as.POSIXct(dates)
+  
+  years <- unique( lubridate::year(dates) )
+  years <- c( min(years)-1, years, years+1 )
  
-  hdays <- holidays( yrs, type=type, silent=TRUE )
+  hdays <- major_dates( years, ... ) 
+    # if( is.null(tags) ) 
+    #  major_dates( years, ... ) else major_dates( years, tags = tags )
+  
+
   m <- outer( dates, hdays, FUN="difftime", units="days" )
   m <- -m
   dys <- apply( m, 1, function(x) min( x[ x>=0 ] ) )
@@ -22,14 +37,18 @@ days_to_holiday <- function( dates, type="NYSE" ) {
   
 }
 
+#' @examples
+#'   days_from_holiday( today() )
+#'   
 #' @export
 #' @rdname date-extras
-days_from_holiday <- function( dates, type="NYSE" ) {
+days_from_holiday <- function( dates, ... ) {
   
+  dates <- as.POSIXct(dates)
   yrs <- unique( year(dates) )
   yrs <- ( min(yrs,na.rm=TRUE)-1 ):(max(yrs, na.rm=TRUE)+1 ) 
  
-  hdays <- holidays( yrs, type, silent=TRUE )
+  hdays <- major_dates( yrs, ... )
 
   if( min(dates, na.rm=TRUE) < min(hdays, na.rm=TRUE) ) 
     warning( "dates exist before earliest recorded holiday" )
@@ -47,7 +66,7 @@ days_from_holiday <- function( dates, type="NYSE" ) {
 #' Create categorical date features 
 #'
 #' @param dates date vector
-#' @param type e.g. "NYSE" (see TimeWarp package)
+#' @param ... additional arguments
 #' @param label how to label the resulting features
 #'  
 #' Calculates various attributes for a given date.  
@@ -56,9 +75,14 @@ days_from_holiday <- function( dates, type="NYSE" ) {
 #' week (week of the year), wday (day of the week), holiday (logical-
 #' whether the day is a holiday, days_to_holiday, days_from_holiday
 #'
+#' @examples  
+#'   featurize_date( now() )
+#'   
 #' @export
 #' @rdname dates-extras
-featurize_date  <- function( dates, type="NYSE", label=NULL ) {
+featurize_date  <- function( 
+  dates, label=NULL, ... 
+) {
   
   feats <- data.frame( 
       date  = dates 
@@ -70,9 +94,9 @@ featurize_date  <- function( dates, type="NYSE", label=NULL ) {
     # , we_days = ( ( 8 - wday(dates) )  %% 7 ) + 1 
     , wday    = lubridate::wday(dates, label=TRUE )
     , we_days = days_to_we_end( dates ) 
-    , is_holiday = isHoliday( dates, type=type)
-    , days_to_holiday = days_to_holiday( dates, type=type )
-    , days_from_holiday = days_from_holiday( dates, type=type )
+    , is_holiday = isHoliday( dates, ... )
+    , days_to_holiday = days_to_holiday( dates, ... )
+    , days_from_holiday = days_from_holiday( dates, ... )
     # , days_to_sunday = ceiling_date(dates, "week") - dates 
   )
   
@@ -98,10 +122,17 @@ featurize_date  <- function( dates, type="NYSE", label=NULL ) {
 #' @param dates Date objects
 #' @param use.holiday Whether to add holidays are the end of
 #' the weekend.
-#' @param type Type of holiday, default: NYSE
+#' @param ... additional arguments passed to \code{isHoliday} such as \code{tags}
 #'
+#' @return the length of time in days between the date and the end of the 
+#' weekend, including long weekends  
+#'
+#' @examples 
+#'  days_to_we_end( now() )
+#'  days_to_we_end( ymd(20140831) )
 #' @export
-days_to_we_end <- function(dates, use.holiday=TRUE, type="NYSE" ) { 
+
+days_to_we_end <- function(dates, use.holiday=TRUE, ... ) { 
 
   we_days = (( 8 - wday(dates) ) %% 7 ) + 1
 
@@ -109,10 +140,49 @@ days_to_we_end <- function(dates, use.holiday=TRUE, type="NYSE" ) {
     monday  <- ceiling_date( dates, "week" ) + ddays(1) 
     tuesday <- ceiling_date( dates, "week" ) + ddays(2) 
 
-    we_days <- ifelse( isHoliday(monday, type=type) , we_days + 1 , we_days )   
-    we_days <- ifelse( isHoliday(monday, type=type) & isHoliday(tuesday, type=type), we_days + 1, we_days ) 
+    we_days <- ifelse( isHoliday(monday, ...) , we_days + 1 , we_days )   
+    we_days <- ifelse( isHoliday(monday, ...) & isHoliday(tuesday, ...), we_days + 1, we_days ) 
   }
 
   return(we_days)
 
 }
+
+#' isHoliday
+#' 
+#' @param dates POXSIXct vector to test
+#' @param tags character; one or more tags indication that 
+#' 
+#' \code{isHoliday} determines whether dates fall on a holiday
+#' 
+#' @return logical of the same length as \code{dates} indicating whether the date
+#'   follows on a holiday
+#' 
+#' @seealso 
+#'   \code{\link[TimeWarp]{isHoliday}}
+#'   
+#' @examples
+#'   isHoliday( ymd( 20140901, 20131225 ) ) 
+#'   isHoliday( now() )
+#'   isHoliday( '20140901' )
+#' 
+#' @export
+
+isHoliday <- function( dates, tags=NULL ) {
+  
+  data(events)
+  
+  if( ! is.POSIXt(dates) ) stop( "dates is not a POSIXct vector" )
+  
+  dates <- round_date( dates, 'day' )
+  
+  years <- unique( lubridate::year(dates) )
+  years <- sort( unique( c( min(years)-1, years, years+1 )  ) ) 
+  
+  hdays <- 
+  if( is.null(tags) ) 
+    major_dates( years ) else major_dates( years, tags = tags )
+  
+  dates %in% hdays 
+  
+}   
