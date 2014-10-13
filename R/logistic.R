eeeedce820e808d7efe8f1218138cc5cd8bf6498
Christopher Brown <cbrown@decisionpatterns.com>
2014-10-13 07:42:27 -0700

    Move R/logistic.R from dp.misc

---
+++ R/logistic.R
@@ -0,0 +1,14 @@
+#' logistic and inverse logistic function
+#' 
+#' apply the logistic function to \code{x}
+#' 
+#' @param x numeric. Argument passed to the logistic functions
+#' @param y numeric. Argument passed to the inverse logistic function
+#' @aliases logistics, logistic_inv, inv.logistic
+#' @export 
+#' @rdname logistic
+logistic <- function(x) 1/(1+exp(-x))
+
+#' @export
+#' @rdname logistic
+logistic_inv <- function(y) -log(1/y-1)
