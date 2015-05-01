c0407624bea147f8c5ee015a68a2432a416f6b37
Christopher Brown <cbrown@decisionpatterns.com>
2015-05-01 15:38:21 -0700

    Commit autokey before moving to data.table.plus

--- R/autokey.R
+++ R/autokey.R
@@ -1,27 +1,35 @@
 #' Automatically set data.table keys
 #' 
-#' Atomatically sets the keys of a data.table based on \code{options}
+#' Atomatically sets the keys of a data.table based on \code{options}.   
 #' 
 #' @param dt data.table
 #' @param keys character; the values to set for keys if present in \code{dt}. 
 #' 
 #' \code{autokey} sets the keys for a data.table. The default is to look at the
-#' \code{getOption('autokey')} for the list available keys.
+#' \code{getOption('autokey')} for the list available keys. It is equivalent
+#' to setting the \code{cols} argument of \code{setkeyv} equivalent to:
+#' 
+#' \code{ intersect( names(x), getOption('autokey') ) }
 #' 
 #' Keys are used by \code{\link{stitch}} for determining how various data 
 #' vectors are related.
-#' 
+#'
+#' @seealso 
+#'   \code{\link[data.table]{setkeyv}} in the \code{data.table} package.
+#'    
 #' @examples
 #'   options( autokey='Species' )
 #'   data(iris)
 #'   setDT(iris)
 #'   autokey(iris)   
+#'   
+#' @import data.table   
 #' @export
 
-  autokey <- function( dt, keys=getOption('autokey') ) {
+  autokey <- function( x, keys=getOption('autokey') ) {
     
-    if( is.data.table(dt) )
-      setkeyv( dt, intersect( names(dt), keys ) )
+    if( data.table::is.data.table(x) )
+      data.table::setkeyv( x, intersect( names(x), keys ) )
   
   }
 
@@ -41,7 +49,7 @@
 #' 
 #' Get the vector of autokeys
 #' 
-#' Simple wrapper of \code{getOption( 'autokey')}
+#' Simple wrapper of \code{getOption('autokey')}
 #' @rdname autokey
 #' @export
   autokeys <- function() getOption( 'autokey')
