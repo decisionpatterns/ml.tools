ffe323f0f556335b4910bba4453618d1a36557a0
Christopher Brown <cbrown@decisionpatterns.com>
2015-12-08 07:52:04 -0800

    Fix docs on logistic function

--- R/logistic.R
+++ R/logistic.R
@@ -4,11 +4,14 @@
 #' 
 #' @param x numeric. Argument passed to the logistic functions
 #' @param y numeric. Argument passed to the inverse logistic function
-#' @aliases logistics, logistic_inv, inv.logistic
+#' @aliases logistic, logistic_inv, inv.logistic
 #' @export 
 #' @rdname logistic
-logistic <- function(x) 1/(1+exp(-x))
+
+  logistic <- function(x) 1/(1+exp(-x))
 
 #' @export
 #' @rdname logistic
-logistic_inv <- function(y) -log(1/y-1)
+
+  logistic_inv <- function(y) -log(1/y-1)
+
